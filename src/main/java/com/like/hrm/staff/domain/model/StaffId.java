79441b38da392e769b0090fd262ced48528a5400
devkbm <devkbm0417@gmail.com>
2024-06-25 22:40:09 +0900

    2024-06-25

--- src/main/java/com/like/hrm/staff/domain/model/StaffId.java
+++ src/main/java/com/like/hrm/staff/domain/model/StaffId.java
@@ -1,52 +1,18 @@
 package com.like.hrm.staff.domain.model;
 
-import java.io.Serializable;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.EqualsAndHashCode;
-/*
-@EqualsAndHashCode(of = {"companyCode", "staffNo"})
-@Embeddable
-public class StaffId implements Serializable {
-
-	private static final long serialVersionUID = 6064949234611151198L;
-
-	@Column(name="ORG_CD")
-	private String companyCode;
-	
-	@Column(name="STAFF_NO")
-	private String staffNo;
-
-	protected StaffId() {}
-	
-	public StaffId(String companyCode, String staffNo) {
-		this.companyCode = companyCode;
-		this.staffNo = staffNo;
-	}
-
-	public String getCompanyCode() {
-		return companyCode;
-	}
-
-	public String getStaffNo() {
-		return staffNo;
-	}		
-}
-*/
 
 @Embeddable
 public record StaffId(
 		@Column(name="ORG_CD") String companyCode,
 		@Column(name="STAFF_NO") String staffNo		
 		) {
-	/*
-	public String getCompanyCode() {
-		return companyCode;
+		
+	public StaffId {
+		Objects.requireNonNull(companyCode);
+		Objects.requireNonNull(staffNo);
 	}
-	
-	public String getStaffNo() {
-		return staffNo;
-	}
-	*/
 }
