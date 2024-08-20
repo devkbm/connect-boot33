53a17bae6e84a573621ce4ee51e9ed021c73d7ff
devkbm <devkbm0417@gmail.com>
2024-08-20 22:32:05 +0900

    2024-08-20

--- src/main/java/com/like/system/login/adapter/in/web/OauthLoginController.java
+++ src/main/java/com/like/system/login/adapter/in/web/OauthLoginController.java
@@ -38,6 +38,9 @@ public class OauthLoginController {
     	this.authorizedClientService = authorizedClientService;
     	this.restTemplateBuilder = restTemplateBuilder;    	
     }
+
+    // TEST URL 
+    // https://accounts.google.com/o/oauth2/auth?client_id=497322312042-mstkseqfmr5t8r7nch5bp17r9lh5eoen.apps.googleusercontent.com&redirect_uri=http://localhost:8090/login/oauth2/code/google&response_type=code&scope=https://www.googleapis.com/auth/admin.directory.resource.calendar.readonly
     
     @GetMapping("/ex")
     public ModelAndView exRedirect1() {
@@ -67,7 +70,7 @@ public class OauthLoginController {
     // 최초 로그인 성공
     // http://localhost:8090/oauth2/authorization/google
     // http://localhost:8090/oauth2/authorization
-    @GetMapping("/loginSuccess")
+    @GetMapping("/loginSuccess")    
     public AuthenticationToken getLoginInfo(OAuth2AuthenticationToken authentication, HttpSession session, HttpServletRequest request) {
     	log.info("/loginSuccess");
     	
