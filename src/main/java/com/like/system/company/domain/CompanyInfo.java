6c7f0ec30e69cf25867b144a5aee25c48a98e2a8
devkbm <devkbm0417@gmail.com>
2024-09-09 22:31:37 +0900

    2024-09-09

--- src/main/java/com/like/system/company/domain/CompanyInfo.java
+++ src/main/java/com/like/system/company/domain/CompanyInfo.java
@@ -2,27 +2,43 @@ package com.like.system.company.domain;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.EmbeddedId;
-import lombok.Getter;
+import com.like.core.jpa.domain.AbstractAuditEntity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access=AccessLevel.PROTECTED)
 @Getter
-public class CompanyInfo {
+@Entity
+@Table(name = "COMCOMPANY")
+public class CompanyInfo extends AbstractAuditEntity {
 
 	@EmbeddedId
 	CompanyInfoId id;
 	
+	@Column(name="ORG_NM")
 	String companyName;
 	
 	// 사업자등록번호
+	@Column(name="BIZ_REG_NO")
 	String businessRegistrationNumber;
 	
 	// 법인번호
+	@Column(name="CORP_NO")
 	String coporationNumber;
 		
 	// 대표자명(성명)
+	@Column(name="REPR_NM")
 	String nameOfRepresentative;
 	
 	// 설립일
+	@Column(name="ESTA_DT")
 	LocalDate establishmentDate;
 
 	public CompanyInfo(String id
